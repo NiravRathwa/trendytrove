69552675e72148a87f6df0fc075200d7fe93fadc
NiravRathwa <niravrathwa10@gmail.com>
2024-06-19 10:11:26 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Hello, Tailwind CSS with React and TypeScript!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
