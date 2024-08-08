d48ebc7a6eadb6a90eacb8e4ea0826d5bc63b23b
NiravRathwa <niravrathwa10@gmail.com>
2024-08-08 15:21:54 +0530

    introduced offLine component

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -9,25 +9,28 @@ import ResetPassword from "../pages/ResetPassword";
 import Home from "../pages/Home";
 import ProductList from "pages/products/ProductList";
 import ProductOverview from "pages/products/ProductOverview";
+import InternetStatus from "components/Internert/InternetStatus";
 const AppRoutes: React.FC = () => {
   const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID as string;
   return (
     <GoogleOAuthProvider clientId={clientId}>
       <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/reset-password/:token" element={<ResetPassword />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<ProductList />} />
-          <Route path="/product/:id" element={<ProductOverview />} />
+        <InternetStatus>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/reset-password/:token" element={<ResetPassword />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<ProductList />} />
+            <Route path="/product/:id" element={<ProductOverview />} />
 
-          {/* <Route path="/about" element={<About />} /> */}
-          {/* <Route path="/contact" element={<Contact />} /> */}
-          {/* <Route path="/404" element={<NotFound />} /> */}
-          {/* <Route path="*" element={<Navigate to="/404" />} /> */}
-        </Routes>
+            {/* <Route path="/about" element={<About />} /> */}
+            {/* <Route path="/contact" element={<Contact />} /> */}
+            {/* <Route path="/404" element={<NotFound />} /> */}
+            {/* <Route path="*" element={<Navigate to="/404" />} /> */}
+          </Routes>
+        </InternetStatus>
       </Router>
     </GoogleOAuthProvider>
   );
